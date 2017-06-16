677270194aba1b64da8f3de33554ac208e48aaff
Mariano Benitez <benitezmariano5@gmail.com>
2017-06-16 10:03:33 -0300

    "vista"

--- www/js/app.js
+++ www/js/app.js
@@ -1,19 +1,48 @@
 (function(){
 
-var app=angular.module('appRss', ['ionic']);
+var app=angular.module('appRss', ['ionic','angularMoment']);
 
-app.controller('appRssCtrl',function($scope){
+app.controller('appRssCtrl',function($http,$scope){
 
-  $scope.noticia=[
-    {
+  $scope.noticia=[];//iniciamos con un arreglo vacio
 
-        title:'Avengers'
-    },
-    {
-       title:'transformers'
-    }    
-  ];
+  function loadNoti(params,callback){
 
+    $http.get('https://www.reddit.com/r/Android/new/.json',{params: params})
+      .success(function(response){
+        var noticia=[];
+      angular.forEach(response.data.children, function(child){
+
+          $scope.noticia.push(child.data);
+          console.log(child.data);
+      });
+      callback(noticia);
+    });
+  }
+
+
+    $scope.loadOlderNoti=function(){
+      var params={};
+
+      if($scope.noticia.length > 0){
+        params['after']=$scope.noticia[$scope.noticia.length - 1].name;
+      }
+      loadNoti(params, function(olderNoti){
+        $scope.noticia=$scope.noticia.concat(olderNoti);
+        $scope.$broadcast('scroll.infiniteScrollComplete');
+
+      });
+
+    };
+    $scope.loadNewNoti=function(){
+      var params={'before':$scope.noticia[0].name};
+      loadNoti(params, function (newNoti) {
+        $scope.noticia=newNoti.concat($scope.noticia);
+        $scope.$broadcast('scroll.refreshComplete');
+      });
+    };
+  //pedimos el object json que queremos descargar
+  //response si llego data
 });
 
 app.run(function($ionicPlatform) {
